39d8ffafe95adf93a56d6681372baadebd8eb57a
Daniel Reis <danielqreis@gmail.com>
2025-09-02 06:54:58 -0300

    fix(routing): add trailing slashes to detail urls
    Adds the required trailing slash to the movie detail URL pattern.
    
    Adds the required trailing slash to the actor detail URL pattern.
    
    This resolves a 404 Not Found error on these endpoints.

--- actors/urls.py
+++ actors/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("actors/", views.ActorCreateListView.as_view(), name="actor-create-list"),
     path(
-        "actors/<int:pk>",
+        "actors/<int:pk>/",
         views.ActorRetrieveUpdateDestroyView.as_view(),
         name="actor-detail-view",
     ),
